bb60be9447fee2fec52d17ffed3be731bf73100a
Paul FIGIEL <pf@homagames.com>
2024-07-26 14:44:40 +0200

    Test

--- src/model/mac-builder.ts
+++ src/model/mac-builder.ts
@@ -1,27 +1,17 @@
-import { getExecOutput } from '@actions/exec';
+import { exec } from '@actions/exec';
 import fs from 'node:fs/promises';
 
 class MacBuilder {
   public static async run(actionFolder: string, silent: boolean = false): Promise<number> {
-    const result = await getExecOutput('bash', [`${actionFolder}/platforms/mac/entrypoint.sh`], {
-      silent,
-    });
-
-    if (result.stdout) {
-      try {
-        await fs.writeFile('unity-execution-logs.log', result.stdout);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    if (result.stderr) {
-      try {
-        await fs.writeFile('unity-execution-logs.log', result.stderr);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    return result.exitCode;
+    const options = {
+      listeners: {
+          stdout: (data: Buffer) => {
+            fs.writeFile('unity-execution-logs.log', data.toString(), { flag: 'a' });
+          }
+      },
+      silent
+  };
+    return await exec('bash', [`${actionFolder}/platforms/mac/entrypoint.sh`], options);
   }
 }
 
